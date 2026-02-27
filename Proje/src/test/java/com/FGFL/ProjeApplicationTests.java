9d883eccae8e55c9775700725b9396c3be60aa6e
Mehmet Sami <karadumanmehmetsami27@gmail.com>
2026-02-27 23:01:55 +0300

    Initial commit

---
+++ Proje/src/test/java/com/FGFL/ProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FGFL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
